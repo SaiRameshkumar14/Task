9afb9df3fa98b7c4d39e77000c3bf7d06f105970
Sai-Ddiwaker-R <sd.designanddevelop@gmail.com>
2025-03-02 15:07:02 +0530

    update

---
+++ 04-Arrays/TransposedMatrix.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class TransposedMatrix {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] transposeArr = new int[arr[0].length][arr.length];
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                transposeArr[i][j] = arr[j][i];
+            }
+        }
+        System.out.println("Transposed Matrix");
+        for (int[] row : transposeArr) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
