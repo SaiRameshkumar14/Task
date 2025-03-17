869d358dd06e3bc3df739163db9f6218f0a2d9a0
SAI DDIWAKER R <102911403+SaiRameshkumar14@users.noreply.github.com>
2025-03-17 13:51:45 +0530

    Update

--- javaAssign/sixClassAndObjects/BookMain.java
+++ javaAssign/sixClassAndObjects/BookMain.java
@@ -1,9 +1,10 @@
 package sixClassAndObjects;
 
 public class BookMain {
-
     public static void main(String[] args) {
+        List<BookDTO> list = new ArrayList<>();
         BookDTO books = new BookDTO("Let's Make IT", "239iljfdw9348SD", "Sam Andrew", 340);
+        list.add(
         books.displayBookInfo();
         System.out.println(BookDTO.getTotalBooks());
     }
