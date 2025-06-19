bd03ed874b90c21b12c0cbce5bca8032801dfcd1
Sai-Ddiwaker-R <sd.designanddevelop@gmail.com>
2025-06-19 14:30:18 +0530

    update

--- javaassign/oneTwopackages/library/books/Book.java
+++ javaassign/oneTwopackages/library/books/Book.java
@@ -1,16 +1,16 @@
+
 package library.books;
 
 public class Book {
-
-    public String title;
-    public String author;
+    private String title;
+    private String author;
 
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
-    public void display() {
-        System.out.println("Book: " + title + " by " + author);
+    public void displayBookInfo() {
+        System.out.println("Title: " + title + ", Author: " + author);
     }
 }
